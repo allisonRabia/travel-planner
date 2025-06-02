7da52aa36ef05c60ee0dab861bd73bee327d9ad8
mariaprang <mprangishvili@newwork.com>
2025-06-02 20:35:41 +0400

    added an endpoint to retrieve "dummy" data for trips

---
+++ src/main/java/app/travelplannerapp/api/TripController.java
@@ -0,0 +1,19 @@
+package app.travelplannerapp.api;
+
+import app.travelplanner.api.TripsApi;
+import app.travelplanner.model.Trip;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TripController implements TripsApi {
+
+    @Override
+    public ResponseEntity<List<Trip>> getAllTrips() {
+        return ResponseEntity.ok(List.of(new Trip("MyTrip", LocalDate.now(), LocalDate.now())));
+    }
+}
